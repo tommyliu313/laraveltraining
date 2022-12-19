575dc4f0ccde850343d750563034025ab4d38b25
tommyliu313 <liuchaktin@gmail.com>
2022-12-19 23:48:25 +0800

    20221219 2348

---
+++ newproject/resources/views/layouts/base.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="bg-gray-100 px-8 py-4 text-gray-700 flex items-center justify-between">
+        <span class="font-bold text-xl">Brand</span>
+        <span>Hello, User</span>
+    </nav>
+    <section class="p-12 mx-auto max-w-6xl text-gray-800">
+        <h1 class="text-4xl font-bold">Home</h1>
+        <p class="mt-4 text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, temporibus corporis nisi et tempora maxime explicabo, expedita vero tempore quidem quod autem sapiente ea reprehenderit? Iste praesentium ratione aspernatur corrupti.
+        </p>
+    </section>
+    
+</body>
+</html>
--- newproject/routes/web.php
+++ newproject/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home',['name' => "Shruti" ]);
+});
+
+Route::get('/test', function(){
+    return "<h1>Welcome</h1>";
+});
+
+Route::get('/anotherhome', function () {
+    return view('anotherhome',['name' => "Shruti" ]);
+});
